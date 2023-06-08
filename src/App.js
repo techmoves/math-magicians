8d56c1f91f454d6c905a5fef58b9e8ff70c91bb7
techmoves <muhndlovu28@gmail.com>
2023-06-08 13:56:40 +0200

    Fix linters error

--- src/App.js
+++ src/App.js
@@ -1,50 +1,46 @@
-import "./App.css";
-import React from "react";
-import Calculator from "./componets/calculator";
-import Input from "./componets/Input";
-import Button from "./componets/button";
+import './App.css';
+import React from 'react';
+import Calculator from './componets/calculator';
+import Input from './componets/Input';
+import Button from './componets/button';
 
-
-const App = () => {
-  return (
-    <div className="App">
-      <div className="calculator-wrapper">
-        <Input />
-        <Button />
-        <div className="row">
-          <Calculator symbol="AC" />
-          <Calculator symbol="+/-" />
-          <Calculator symbol="%" />
-          <Calculator symbol="+" Color="#f2a33c" />
-        </div>
-        <div className="row">
-          <Calculator symbol="7" />
-          <Calculator symbol="8" />
-          <Calculator symbol="9" />
-          <Calculator symbol="X" Color="#f2a33c" />
-        </div>
-        <div className="row">
-          <Calculator symbol="4" />
-          <Calculator symbol="5" />
-          <Calculator symbol="6" />
-          <Calculator symbol="-" Color="#f2a33c" />
-        </div>
-        <div className="row">
-          <Calculator symbol="1" />
-          <Calculator symbol="2" />
-          <Calculator symbol="3" />
-          <Calculator symbol="+" Color="#f2a33c" />
-        </div>
-        <div className="row">
-          <Calculator symbol="0" />
-          <Calculator symbol="" />
-          <Calculator symbol="." />
-          <Calculator symbol="=" Color="#f2a33c" />
-        </div>
+const App = () => (
+  <div className="App">
+    <div className="calculator-wrapper">
+      <Input />
+      <Button />
+      <div className="row">
+        <Calculator symbol="AC" />
+        <Calculator symbol="+/-" />
+        <Calculator symbol="%" />
+        <Calculator symbol="+" Color="#f2a33c" />
+      </div>
+      <div className="row">
+        <Calculator symbol="7" />
+        <Calculator symbol="8" />
+        <Calculator symbol="9" />
+        <Calculator symbol="X" Color="#f2a33c" />
+      </div>
+      <div className="row">
+        <Calculator symbol="4" />
+        <Calculator symbol="5" />
+        <Calculator symbol="6" />
+        <Calculator symbol="-" Color="#f2a33c" />
+      </div>
+      <div className="row">
+        <Calculator symbol="1" />
+        <Calculator symbol="2" />
+        <Calculator symbol="3" />
+        <Calculator symbol="+" Color="#f2a33c" />
+      </div>
+      <div className="row">
+        <Calculator symbol="0" />
+        <Calculator symbol="" />
+        <Calculator symbol="." />
+        <Calculator symbol="=" Color="#f2a33c" />
       </div>
     </div>
-  );
-
-};
+  </div>
+);
 
 export default App;
