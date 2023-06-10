d1a7f32b02502632d4e812fb20412019c420da94
techmoves <muhndlovu28@gmail.com>
2023-06-10 23:32:11 +0200

    Fix bug

--- src/componets/qoutes.js
+++ src/componets/qoutes.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
 
 export default function Quote() {
   const [data, setData] = useState(null);
@@ -9,16 +9,15 @@ export default function Quote() {
     const fetchData = async () => {
       setLoading(true);
 
-
       const settings = {
-        headers: { "X-Api-Key": "5yCNhWqtixQuin+kzUdPQg==tAtXV9GzU1F6kFYv" },
-        contentType: "application/json",
-        method: "GET",
+        headers: { 'X-Api-Key': '5yCNhWqtixQuin+kzUdPQg==tAtXV9GzU1F6kFYv' },
+        contentType: 'application/json',
+        method: 'GET',
       };
       try {
         const response = await fetch(
-          "https://api.api-ninjas.com/v1/quotes?category=success",
-          settings
+          'https://api.api-ninjas.com/v1/quotes?category=success',
+          settings,
         );
         const responsJson = await response.json();
         setData(`${responsJson[0].quote}`);
@@ -38,5 +37,11 @@ export default function Quote() {
   if (loading === true) return <div>Loading...</div>;
   if (hasError === true) return <div>Error please try again</div>;
 
-  return <div className="load-data"> {data} </div>;
+  return (
+    <div className="load-data">
+      {' '}
+      {data}
+      {' '}
+    </div>
+  );
 }
