58a5db92617debb9c028e6e95beb3b6d6f4bfd59
techmoves <muhndlovu28@gmail.com>
2023-06-15 11:33:00 +0200

    Add test

--- src/test/componets.test.js
+++ src/test/componets.test.js
@@ -1,6 +1,26 @@
-import React from "react";
 import renderer from "react-test-renderer";
-import Link from "../Link.react";
-// import Button from "../componets/button";
-// import calculate from "../logic/calculate";
+import Link from "../Link";
 
+it("changes the class when hovered", () => {
+  const component = renderer.create(
+    <Link page="http://www.facebook.com">Facebook</Link>
+  );
+  let tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+
+  // manually trigger the callback
+  renderer.act(() => {
+    tree.props.onMouseEnter();
+  });
+  // re-rendering
+  tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+
+  // manually trigger the callback
+  renderer.act(() => {
+    tree.props.onMouseLeave();
+  });
+  // re-rendering
+  tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
