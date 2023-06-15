3a8e60c7a024357235e07c6dd511fa4ac891dc47
techmoves <muhndlovu28@gmail.com>
2023-06-15 12:21:37 +0200

    Updated home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -7,20 +7,32 @@ function Home() {
         <h1 className="title"> Welcome to our page!</h1>
 
         <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero
-          itaque aspernatur eligendi optio a veniam sapiente deserunt totam
-          assumenda aut, ab nemo suscipit ratione repudiandae doloremque
-          distinctio vitae reiciendis! Dolor sit voluptatem ullam necessitatibus
-          ducimus iure voluptates natus ad aspernatur? Quia quae voluptatibus
-          neque repellat quos magnam suscipit, corporis fugit.
+          Welcome to Math Magician, where math becomes magical! Our website is
+          dedicated to helping children and students of all ages improve their
+          arithmetic skills and develop a love for mathematics. Math Magician is
+          a fun and educational game that challenges players to solve a series
+          of math problems in a race against the clock. With three difficulty
+          levels to choose from, players can practice their basic addition,
+          subtraction, multiplication, and division skills while having fun and
+          competing with their friends.But Math Magician is more than just a
+          game. Its a powerful tool for improving your math skills and building
+          your confidence in mathematics. By practicing your arithmetic skills
+          regularly, you will be able to solve more complex math problems with
+          ease and tackle new challenges with confidence.
         </p>
         <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Libero
-          itaque aspernatur eligendi optio a veniam sapiente deserunt totam
-          assumenda aut, ab nemo suscipit ratione repudiandae doloremque
-          distinctio vitae reiciendis! Dolor sit voluptatem ullam necessitatibus
-          ducimus iure voluptates natus ad aspernatur? Quia quae voluptatibus
-          neque repellat quos magnam suscipit, corporis fugit.
+          Welcome to Math Magician, where math becomes magical! Our website is
+          dedicated to helping children and students of all ages improve their
+          arithmetic skills and develop a love for mathematics. Math Magician is
+          a fun and educational game that challenges players to solve a series
+          of math problems in a race against the clock. With three difficulty
+          levels to choose from, players can practice their basic addition,
+          subtraction, multiplication, and division skills while having fun and
+          competing with their friends.But Math Magician is more than just a
+          game. Its a powerful tool for improving your math skills and building
+          your confidence in mathematics. By practicing your arithmetic skills
+          regularly, you will be able to solve more complex math problems with
+          ease and tackle new challenges with confidence.
         </p>
       </div>
     </div>
