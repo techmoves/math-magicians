409be9e877ac336f2cd05863eedc2e3dc19222f4
techmoves <muhndlovu28@gmail.com>
2023-06-15 12:16:54 +0200

    Fix linters

--- src/test/calculate.test.js
+++ src/test/calculate.test.js
@@ -1,39 +1,39 @@
-import calculate from "../logic/calculate";
+import calculate from '../logic/calculate';
 
-describe("test for calculate js file", () => {
-  test("when AC button is clicked", () => {
+describe('test for calculate js file', () => {
+  test('when AC button is clicked', () => {
     const output = calculate(
       {
-        total: "6",
-        next: "2",
-        operation: "",
+        total: '6',
+        next: '2',
+        operation: '',
       },
-      "AC"
+      'AC',
     );
     expect(output.total).toEqual(null);
   });
 
-  test("when - button is clicked", () => {
+  test('when - button is clicked', () => {
     const output = calculate(
       {
-        total: "6",
-        next: "4",
-        operation: "-",
+        total: '6',
+        next: '4',
+        operation: '-',
       },
-      "-"
+      '-',
     );
-    expect(output.total).toEqual("2");
+    expect(output.total).toEqual('2');
   });
 
-  test("when + button is clicked", () => {
+  test('when + button is clicked', () => {
     const output = calculate(
       {
-        total: "6",
-        next: "4",
-        operation: "+",
+        total: '6',
+        next: '4',
+        operation: '+',
       },
-      "+"
+      '+',
     );
-    expect(output.total).toEqual("10");
+    expect(output.total).toEqual('10');
   });
 });
